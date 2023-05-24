eea79f2ab9d99c60fce7cd2591ac9b6286a6c0e5
HW <hamiltonwei@gmail.com>
2023-05-24 01:00:13 -0400

    got a basic graph showing on the webpage!!! YAAAYYY!!!!

--- Project/js/musicVolChartScript.js
+++ Project/js/musicVolChartScript.js
@@ -2,21 +2,29 @@
 /**
  * 
  * Initialize the X and Y scale of the data.
- * @param {*} data 
- * @param {*} dimensions 
- * 
+ * @param {*} data the dataset we need
+ * @param {*} dimensions the dimensions of the canvas
+ * @param {*} max The maximum of the data set
  */
-function initializeScale(data, dimensions){
-    var x = d3.scaleLinear()
-              .domain(d3.extent(data, function(d) { return d.year; }))
+function initializeScale(data, dimensions, max=999999999){
+    function convertToDate(d) {
+        var dateobj = new Date(d["Date"]);
+        return dateobj;
+    }
+
+    // the domain is a Date object, NOT a string. You can't pass in a string here!
+    var xdmn = d3.extent(data, convertToDate);
+    var x = d3.scaleTime()
+              .domain(xdmn) 
               .range([ 0, dimensions.width ]);
 
     var y = d3.scaleLinear()
-              .domain([0, 200000])
+              .domain([0, max])
               .range([ dimensions.height, 0 ]);
 
     return {xScale: x, yScale: y}
 }
+
 
 /**
  * Add the x and y axis of the SVG canvas
@@ -67,6 +75,7 @@ function initializeSVG(margin, dimensions){
  * 
  */
 function stackData(data, keys){
+    var keys = keys.slice(1,3)
     var stacker = d3.stack().keys(keys);
     var stackedDataTemp = stacker(data);
     var stackedData = []
@@ -74,9 +83,9 @@ function stackData(data, keys){
         var singleStack = []; // a single "stack" of the data.
         for (const element of singleStackTemp){
             singleStack.push({
-            year: element.data.year, 
+            date: element.data.Date, 
             name: singleStackTemp.key,
-            value: element.slice(0,3)});
+            values: element.slice(0,3)});
         }
         stackedData.push(singleStack);
     }
@@ -92,10 +101,26 @@ function stackData(data, keys){
  * 
  */
 function displayArea(stackedData, color, xScale, yScale){
+    function x_gen(d){
+        var date = new Date(d.date)
+        var converted = xScale(date);
+        return converted;
+    } 
+
+    function y0_gen(d){
+        var v = yScale(d.values[0]);
+        return v;
+    }
+
+    function y1_gen(d){
+        var v = yScale(d.values[1]);
+        return v;
+    }
+
     var areaGenerator = d3.area()
-        .x(function(d) { return xScale(d.year)})
-        .y0(function(d) { return yScale(d.value[0]) })
-        .y1(function(d) { return yScale(d.value[1]) });
+        .x(x_gen)
+        .y0(y0_gen)
+        .y1(y1_gen);
     
     // Show the areas
     svg
@@ -108,10 +133,13 @@ function displayArea(stackedData, color, xScale, yScale){
 }
 
 
+
+
+
 // set the dimensions and margins of the graph
 var dimensions = {height: 666, width: 1000};
 var margin = {top: 20, right: 30, bottom: 30, left: 60};
-var csvPath = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv";
+var csvPath = "processed_data/With_New_Scrubbles.csv";
 
 // append the svg object to the body of the page
 var svg = initializeSVG(margin, dimensions);
@@ -119,13 +147,19 @@ var svg = initializeSVG(margin, dimensions);
 //Read the data
 d3.csv(csvPath, function(data) {
     var keys = data.columns.slice(1);
-    // color palette
+    // color paletteL
     var color = d3.scaleOrdinal()
         .domain(keys)
         .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']);
-    var scales = initializeScale(data, dimensions);
-    addXYAxis(svg, dimensions, scales.xScale, scales.yScale);
-
-    var stackedData = stackData(data, keys)
+    var stackedData = stackData(data, keys);
+    
+    var one_col = stackedData[stackedData.length - 1]
+    function extract_num(d) {
+        var data = d.values[1];
+        return data;
+    }
+    var max = d3.max(one_col, extract_num); //TODO: Max calculation is wrong here
+    var scales = initializeScale(data, dimensions, max);
+    addXYAxis(svg, dimensions, scales.xScale, scales.yScale); 
     displayArea(stackedData, color, scales.xScale, scales.yScale)
 })
